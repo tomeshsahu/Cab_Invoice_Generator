e76cc7b44b3c330d0e56567596fa53f77f31475f
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-06-03 13:56:46 +0530

    [TOMESHWAR]ADD:-GIT COMMIT INVOICE GENERATOR

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,7 @@
+﻿using CabInvoiceGenerator;
+
+Console.WriteLine("Welcome to Cab Invoice Generator");
+InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+double fare = invoiceGenerator.CalculateFare(2.0, 5);
+Console.WriteLine($"Fare : {fare}");
+Console.ReadLine();
